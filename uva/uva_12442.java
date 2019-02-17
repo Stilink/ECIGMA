ab5ca48182b06fac250e4edeec85bd32d48a5a83
Jeymar Vega <jeymar.vega@mail.escuelaing.edu.co>
2019-02-17 11:38:01 -0500

    Add files via upload

--- uva/uva_12442.java
+++ uva/uva_12442.java
@@ -1,9 +1,12 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
 public class uva_12442 {
-	private static int[] clan;
-	private static boolean[] vi;
+	private static int[] clan = new int[50050];
+	private static boolean[] vi = new boolean[50050];
+	private static int[] resp = new int[50050];
 	private static int dfs(int s) {
 		vi[s]=true;
 		int nodosAlcanzados = 1;
@@ -11,8 +14,8 @@ public class uva_12442 {
 			nodosAlcanzados+=dfs(clan[s]);
 		}
 		vi[s]=false;
-		return nodosAlcanzados;
-	}
+		return resp[s]=nodosAlcanzados;
+		}
 	
 	
 	public static void main(String[] args) throws Exception{
@@ -21,8 +24,6 @@ public class uva_12442 {
 		int cases = Integer.parseInt(br.readLine());
 		for(int c = 1; c<=cases; c++) {
 			int n = Integer.parseInt(br.readLine());
-			clan = new int[n];
-			vi = new boolean[n];
 			StringTokenizer stk;
 			for(int i = 0; i <n; i++) {
 				stk = new StringTokenizer(br.readLine());
@@ -30,17 +31,21 @@ public class uva_12442 {
 				int v = Integer.parseInt(stk.nextToken());
 				clan[u-1]=v-1;
 				vi[u-1]=false;
+				resp[u-1]=Integer.MAX_VALUE;
 			}
 			int maximo = Integer.MIN_VALUE;
 			int nodo = Integer.MAX_VALUE;
 			for(int i=0; i<n;i++ ) {
-				int res = dfs(i);
-				if(res>maximo) {
-					maximo=res;
-					nodo=i;
-				}else if(res==maximo) {
-					nodo=Math.min(i, nodo);
+				if(resp[i]==Integer.MAX_VALUE) {
+					int res = dfs(i);
+					if(res>maximo) {
+						maximo=res;
+						nodo=i;
+					}else if(res==maximo) {
+						nodo=Math.min(i, nodo);
+					}
 				}
+				
 			}
 			out.println("Case "+c+": "+(nodo+1));
 			
